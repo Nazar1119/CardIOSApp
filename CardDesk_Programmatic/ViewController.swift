606cc1c549240ff2318589326b75346a8ccd774d
Nazar1119 <nazaruhka2004@gmail.com>
2024-09-28 20:28:02 +0200

    Initial Commit

---
+++ CardDesk_Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardDesk_Programmatic
+//
+//  Created by Тимочко Назар on 28/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
